3718f26889fff709396a5e6014248d1e0aaf76ab
Palak Patel <paltel1994@gmail.com>
2025-04-26 15:41:42 -0400

    Sql script

--- Sqlite_Interface.py
+++ Sqlite_Interface.py
@@ -2,3 +2,33 @@ import sqlite3
 
 con = sqlite3.connect("animegress.db")
 
+cursor = con.cursor()
+
+#create a dummy table
+cursor.execute(
+    """
+               CREATE Table anime (
+               name TEXT,
+               total_episodes INTEGER,
+               genre TEXT
+            )
+"""
+)
+
+Anime_list = [
+    ('Attack on Titan', 75, "Action")
+    ("Naruto", 225, "Action")
+    ("Bleach", 500, "Action")
+    ("JoJo's Bizarre Adventure", 52, "Action")
+    ("Working!!!", 24, "Comedy")
+]
+
+cursor.executemany(
+    """
+    INSERT into anime (name, total_episodes, genre)
+    Values(?, ?, ?)
+    """,
+    Anime_list
+)
+
+con.commit()
