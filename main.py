b5ce3f26d967d0453923107e85e392ac1cdb411e
Palak Patel <paltel1994@gmail.com>
2025-04-27 18:41:53 -0400

    review changes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ while True:
 
     match choice:
             case "1":
-                anime_name = input("Enter the anime name: ").strip
+                anime_name = input("Enter the anime name: ").strip()
 
                 try:    
                     total_episodes = int(input("Enter the total number of episodes: "))
@@ -26,9 +26,11 @@ while True:
                 
                 if total_episodes < 0 or watched_episodes < 0:
                     print("cant be less than 0")
+                    continue
 
                 if watched_episodes > total_episodes:
                     print("cant be more than total episodes watched")
+                    continue
 
                 this_anime = anime.add_anime(anime_name, total_episodes, watched_episodes) 
 
@@ -36,14 +38,24 @@ while True:
                 print(this_anime)
 
             case "2":
-               anime_name = input("Enter the anime name: ")
-               watched_episodes = int(input("Enter the number of episodes you have watched: "))
+               anime_name = input("Enter the anime name: ").strip()
+               
+               try:    
+                    watched_episodes = int(input("Enter the number of episodes you have watched: "))
+               except ValueError:
+                    print("total episodes must be numbers")
+                    continue
+               
+               if watched_episodes < 0:
+                    print("cant be less than 0")
+                    continue
+               
                updated_anime = anime.update_anime(anime_name,watched_episodes)
                print("anime updated sucessfully")
                print(updated_anime)
 
             case "3":
-                anime_name = input("Enter the anime name: ")
+                anime_name = input("Enter the anime name: ").strip()
                 deleteanime = anime.delete_anime(anime_name)
                 print("anime deleted")
                 print(deleteanime)
@@ -52,7 +64,7 @@ while True:
                 print("use 1 or 2 or 3 punk ass bitch")
 
 
-    continue_choice = input("Do you want to continue adding anime? (Y/N): ")
+    continue_choice = input("Do you want to continue adding anime? (Y/N): ").strip().lower()
     if continue_choice != ("y"):
         print("no more anime added")
         break
