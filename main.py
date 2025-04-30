caeb5396e10f981d2271062c1ef91b22f644bc04
Palak Patel <paltel1994@gmail.com>
2025-04-29 22:14:00 -0400

    updated grammer

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ while True:
                 try:    
                     total_episodes = int(input("Enter the total number of episodes: "))
                     if (total_episodes < 1 or total_episodes > 3000):
-                        print("total episodes must be between 1 and 3000")
+                        print("Total episodes must be between 1 and 3000.")
                         continue
                 except ValueError:
-                    print("total episodes must be numbers")
+                    print("Total episodes must be numbers.")
                     continue
 
 
                 try:
                     watched_episodes = int(input("Enter the number of episodes you have watched: "))
                     if (watched_episodes < 0 or watched_episodes > 3000):
-                        print("total episodes must be between 0 and 3000")
+                        print("Total episodes entered must be between 0 and 3000")
                         continue
                 except ValueError:
-                    print("total episodes must be numbers")
+                    print("Total episodes must be numbers.")
                     continue
                 
                        #not needed anymore since previous condition checks for this 
@@ -58,10 +58,10 @@ while True:
                try:    
                     watched_episodes = int(input("Enter the number of episodes you have watched: "))
                     if (watched_episodes < 0 or watched_episodes > 3000):
-                        print("total episodes must be between 1 and 3000")
+                        print("Total episodes must be between 1 and 3000")
                         continue
                except ValueError:
-                    print("total episodes must be numbers")
+                    print("Total episodes must be numbers")
                     continue
                
         #       if watched_episodes < 0:
